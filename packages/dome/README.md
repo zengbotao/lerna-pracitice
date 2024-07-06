c1d05e789e561ec532ba5d252aedad4959238e59
zengbotao <2898487084@qq.com>
2024-07-06 16:35:06 +0800

    pnpm

---
+++ packages/dome/README.md
@@ -0,0 +1,11 @@
+# `dome`
+
+> TODO: description
+
+## Usage
+
+```
+const dome = require('dome');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/dome/__tests__/dome.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const dome = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(dome(), 'Hello from dome');
+console.info('dome tests passed');
