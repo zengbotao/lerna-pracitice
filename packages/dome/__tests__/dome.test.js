986a1a4444ad768f52ce8ec3bf119c8fe9d51aed
zengbotao <2898487084@qq.com>
2024-07-07 09:55:01 +0800

    去除2个包和文件校验

--- packages/dome/__tests__/dome.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const dome = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(dome(), 'Hello from dome');
-console.info('dome tests passed');
--- packages/module-1/index.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Description:
- * @Autor: zengbotao@myhexin.com
- * @Date: 2024-07-06 16:08:22
- * @LastEditTime: 2024-07-06 16:55:26
- */
-module.export = function() {
-  console.log(1);
-};
