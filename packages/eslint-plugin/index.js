1d8cb17001f9149310734976046d8ce1f790d27d
zengbotao <2898487084@qq.com>
2024-07-11 09:35:52 +0800

    fix: eslintplugin

--- packages/eslint-plugin/index.js
+++ packages/eslint-plugin/index.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const requireAll = require('require-all');
-
+//参考https://eslint.org/docs/latest/extend/plugins自定义插件
+// meta - 有关插件的信息。
+// configs - 包含命名配置的对象。
+// rules - 包含自定义规则定义的对象。
+// processors - 包含命名处理器的对象。
 exports.rules = requireAll({
   dirname: path.resolve(__dirname, 'rules'),
 });
--- packages/eslint-plugin/rules/no-http-url.js
+++ packages/eslint-plugin/rules/no-http-url.js
@@ -1,25 +1,59 @@
 const RULE_NAME = 'no-http-url';
-
+//参考https://eslint.org/docs/latest/extend/custom-rule-tutorial自定义规则
 module.exports = {
   name: RULE_NAME,
   meta: {
     type: 'suggestion',
     fixable: null,
-    messages: {
-      noHttpUrl: 'Recommended "{{url}}" switch to HTTPS',
-    },
   },
   create(context) {
     return {
+      //定义规则的 create 函数，该函数接受 context 对象并返回一个对象，
+      //其中包含要处理的每个语法节点类型的属性。
+      //在本例中，您想要处理 Literal 字面量节点。您可以选择任何 ESTree 节点类型或选择器。
+      //https://www.jianshu.com/p/b3f1ff0b3cdf ESTree 节点类型
+      // {
+      //   type: 'Literal',
+      //   loc: SourceLocation {
+      //     start: Position { line: 1, column: 11 },
+      //     end: Position { line: 1, column: 34 }
+      //   },
+      //   range: [ 11, 34 ],
+      //   value: 'https://chenghuai.com',
+      //   raw: "'https://chenghuai.com'",
+      //   parent: <ref *1> Node {
+      //     type: 'VariableDeclarator',
+      //     loc: SourceLocation { start: [Position], end: [Position] },
+      //     range: [ 4, 34 ],
+      //     id: Node {
+      //       type: 'Identifier',
+      //       loc: [SourceLocation],
+      //       range: [Array],
+      //       name: 'test',
+      //       parent: [Circular *1]
+      //     },
+      //     init: [Circular *2],
+      //     parent: Node {
+      //       type: 'VariableDeclaration',
+      //       loc: [SourceLocation],
+      //       range: [Array],
+      //       declarations: [Array],
+      //       kind: 'var',
+      //       parent: [Node]
+      //     }
+      //   }
+      // }
       Literal: function handleRequires(node) {
+       
         if (node.value && typeof node.value === 'string' && node.value.indexOf('http:') === 0) {
           context.report({
             node,
-            messageId: 'noHttpUrl',
+            message: "Recommended '{{url}} 'switch to HTTPS",//meta中定义的数据
             data: {
               url: node.value,
             },
           });
+           console.log(node);
         }
       },
     };
